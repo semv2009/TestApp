7bb07f275f9dab95e4fcee853dd94e0b607258b4
semv2009 <developer@Mac-mini-developer.local>
2016-05-05 12:53:28 +0600

    данные

---
+++ TestApp/ViewControllers/Loading/LoadingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingViewController.swift
+//  ToDoList
+//
+//  Created by developer on 07.04.16.
+//  Copyright © 2016 developer. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
